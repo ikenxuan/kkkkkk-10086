cf8f789c4f84e9e9b155305bb58498153570ca0f
kkkkkkxuan <2066855608@qq.com>
2023-01-31 01:50:10 +0800

    add 手写作业

---
+++ apps/手写文本.js
@@ -0,0 +1,32 @@
+import plugin from '../../../lib/plugins/plugin.js'
+import { segment } from 'oicq'
+
+export class wenan extends plugin {
+    constructor () {
+      super({
+        name: '手写文本',
+        dsc: '手写',
+        event: 'message',
+        priority: 5000,
+        rule: [
+          {
+            reg: `^(手写)(.*)`,
+            fnc: 'realtext'
+          },
+        ]
+      })
+    }
+    
+    async realtext(e) {
+        let msg = e.msg
+		let text = msg.replace(/#|手写/g, "").trim();
+        let url = `https://zj.v.api.aa1.cn/api/zuoye/?msg=${text}`;
+        let data = [
+            // segment.at(e.user_id),
+            segment.image(url),
+        ]
+      //发送消息
+      e.reply(data);
+
+    }
+}
