44eab68121f1dcae26f135be685ad3d7aff25895
kkkkkk <11660834+kkkkkkxuan@user.noreply.gitee.com>
2023-03-15 12:56:45 +0000

    update apps/手写文本.js.
    Signed-off-by: kkkkkk <11660834+kkkkkkxuan@user.noreply.gitee.com>

--- apps/手写文本.js
+++ apps/手写文本.js
@@ -1,6 +1,7 @@
 import plugin from '../../../lib/plugins/plugin.js'
 import ArkMsg from '../model/ArkMsg.js'
 import { segment } from 'oicq'
+const _path = process.cwd()
 
 export class wenan extends plugin {
     constructor () {
@@ -36,7 +37,7 @@ export class wenan extends plugin {
         async dt(e) {
         this.e.isGroup = true
         this.e.group = Bot.pickGroup(e.group_id)
-        let imgpath = `${_path}/plugins/kkkkkk-10086/resources/开团.jpg`
+        let imgpath = `${_path}/plugins/kkkkkk-10086/resources/鸽鸽的照片/开团.jpg`
         let imgurl = await ArkMsg.upload_image(imgpath, true)
         let card_json = ArkMsg.ShareImage_JSON(imgurl)
         console.log(card_json)
