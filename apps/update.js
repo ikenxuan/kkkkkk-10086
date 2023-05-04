d7cbeb407e7046a1c8a97b9e22686567486a13b8
kkkkkk <11660834+kkkkkkxuan@user.noreply.gitee.com>
2023-05-04 22:48:20 +0800

    f

--- apps/update.js
+++ apps/update.js
@@ -1,10 +1,9 @@
 import plugin from "../../../lib/plugins/plugin.js";
-import { createRequire } from "module";
 import lodash from "lodash";
 import fs from 'fs'
 import { Restart } from '../../other/restart.js'
-const require = createRequire(import.meta.url);
-const { exec, execSync } = require("child_process");
+import { exec, execSync } from "child_process";
+import { promisify } from "util";
 let uping = false;
 export class update extends plugin {
     constructor() {
