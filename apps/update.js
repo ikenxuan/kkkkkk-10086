3f8c3943ffe1d9c73d8c4719537cd4f3b50d90b2
kkkkkk <11660834+kkkkkkxuan@user.noreply.gitee.com>
2023-05-04 23:20:44 +0800

    a

--- apps/update.js
+++ apps/update.js
@@ -54,6 +54,12 @@ export class update extends plugin {
 
     }
 
+    // 使用示例
+    //const repoUrl = 'https://gitee.com/ikenxuan/kkkkkk-10086-resources.git';
+    //const localPath = path.join(__dirname, 'plugins', 'kkkkkk-10086', 'resources');
+
+
+
     async update() {
         if (!this.e.isMaster) return false;
         if (uping) {
