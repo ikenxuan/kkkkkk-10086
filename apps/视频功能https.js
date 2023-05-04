bd7691a971e9b2d8f3df35673fef11121e8f6208
kkkkkk <11660834+kkkkkkxuan@user.noreply.gitee.com>
2023-05-04 16:13:25 +0800

    fix

--- apps/视频功能https.js
+++ apps/视频功能https.js
@@ -103,7 +103,7 @@ export class example extends plugin {
     })
     let xhs_note_json = await xhs_fetch.json();
     if (xhs_note_json.hasOwnProperty('detail') || xhs_note_json.detail?.status === false) {
-      logger.error(logger.red(`请尝试获取新的TikHub账号！因为${xhs_note_json.detail.message}`) + '，可前往' + logger.blue('https://dash.tikhub.io/pricing' + '购买额外请求次数或者' + logger.green('注册新账号')));
+      logger.error(logger.red(`请尝试获取新的TikHub账号！因为${xhs_note_json.detail.message}`) + '，可前往' + logger.blue('https://dash.tikhub.io/pricing ' + ' 购买额外请求次数或者' + logger.green('注册新账号')));
       return true;
     } else {
       logger.info('TikHub API' + logger.green('请求成功') + '，正在获取笔记：' + logger.yellow(longLink) + '的数据')
@@ -130,7 +130,7 @@ export class example extends plugin {
     let xhs_tags = []
     let xhs_tags_text = []
     for (let i = 0; i < xhs_note_json.data.image_list.length; i++) {
-    let tag = xhs_note_json.data.tag_list[i].name
+    let tag = xhs_note_json.data.tag_list[i]?.name
     logger.info(tag)
     xhs_tags_text.push(tag)
 
@@ -156,7 +156,7 @@ export class example extends plugin {
     let data = await sharedata.json();
     //logger.info(data)
     if (data.hasOwnProperty('detail') || data.detail?.status === false) {
-      logger.error(logger.red(`请尝试获取新的TikHub账号！因为${data.detail.message}`) + '，可前往' + logger.blue('https://dash.tikhub.io/pricing' + '购买额外请求次数或者' + logger.green('注册新账号')));
+      logger.error(logger.red(`请尝试获取新的TikHub账号！因为${data.detail.message}`) + '，可前往' + logger.blue('https://dash.tikhub.io/pricing ' + ' 购买额外请求次数或者' + logger.green('注册新账号')));
       return true;
     } else {
       logger.info('TikHub API' + logger.green('请求成功') + '，正在获取视频：' + logger.yellow(URL) + '的数据')
@@ -598,6 +598,10 @@ export class example extends plugin {
       headers: headers2
     });
     let notedayjson = await noteday.json();
+    await fetch (`https://api.tikhub.io/promotion/claim?promotion_id=1`, {
+      method: "GET",
+      headers: headers2
+    })
     //logger.mark(notedayjson);
     if (notedayjson.status === true) {
       logger.info(notedayjson.message)
