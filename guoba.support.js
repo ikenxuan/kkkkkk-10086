45a542bf42987cca9217b04a53ac965b635763d7
kkkkkk <11660834+kkkkkkxuan@user.noreply.gitee.com>
2023-05-29 03:12:46 +0800

    fix: 添加可选接口

--- guoba.support.js
+++ guoba.support.js
@@ -16,7 +16,7 @@ export function supportGuoba() {
             description: '提供了视频解析功能，额外的语音盒资源。自用练手项目',
             // 显示图标，此为个性化配置
             // 图标可在 https://icon-sets.iconify.design 这里进行搜索
-            icon: 'emojione-v1:face-savoring-food',
+            icon: 'skill-icons:nodejs-light',
             // 图标颜色，例：#FF0000 或 rgb(255, 0, 0)
             iconColor: '#00c3ff'
         },
@@ -27,7 +27,7 @@ export function supportGuoba() {
                 {
                     field: 'account',
                     label: 'TikHub 账号',
-                    bottomHelpMessage: '可到 https://api.tikhub.io/#/Authorization/register_user_users_register__post 注册账号',
+                    bottomHelpMessage: '可到 https://dash.tikhub.io/signin?next=/dashboard 注册账号',
                     component: 'Input',
                     required: false,
                     componentProps: {
@@ -57,7 +57,7 @@ export function supportGuoba() {
                 {
                     field: 'address',
                     label: '抖音解析服务地址',
-                    bottomHelpMessage: '没有部署可不填，将会使用在线接口',
+                    bottomHelpMessage: '没有部署可不填，将会默认使用在线接口或使用作者的：douyin.realkkkkkk.asia',
                     component: 'Input',
                     componentProps: {
                         placeholder: '127.0.0.1:8000 或者 IP+端口号 或 域名'
