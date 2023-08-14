5cb0939d5cdf8b879b19956526bc41da86ff30df
kkkkkk <11660834+kkkkkkxuan@user.noreply.gitee.com>
2023-08-15 06:15:58 +0800

    优化设置

--- apps/admin.js
+++ apps/admin.js
@@ -3,52 +3,60 @@ import fs from 'fs/promises'
 
 let configPath = process.cwd() + '/plugins/kkkkkk-10086/config/config.json'
 
+async function updateConfig(key, value, e) {
+  const str = await fs.readFile(configPath, 'utf8')
+  const config = JSON.parse(str)
+
+  config[key] = value
+
+  await fs.writeFile(configPath, JSON.stringify(config, null, 2))
+  e.reply('设置成功！')
+}
+
+function getValue(msg) {
+  if (msg.includes('开启')) return true
+  if (msg.includes('关闭')) return false
+}
+
+
 export class admin extends plugin {
-    constructor(e) {
-        super({
-            name: 'kkkkkk-10086-管理',
-            dsc: 'admin',
-            event: 'message',
-            priority: 5000,
-            rule: [
-                {
-                    reg: '^#*(KKK|kkk|kkkkkk-10086)设置语音盒(开启|关闭)$',
-                    fnc: 'voicebox'
-                },
-                {
-                    reg: '^#*(KKK|kkk|kkkkkk-10086)设置(视频解析|解析)(开启|关闭)$',
-                    fnc: 'tools'
-                }
-            ]
-        })
-    }
 
-    async voicebox(e) {
-        if (e.msg.includes('开启')) {
-            const str = await fs.readFile(configPath, 'utf-8')
-            const config = JSON.parse(str)
-            config.voicebox = true
-            await fs.writeFile(configPath, JSON.stringify(config, null, 2))
-        } else if (e.msg.includes('关闭')) {
-            const str = await fs.readFile(configPath, 'utf-8')
-            const config = JSON.parse(str)
-            config.voicebox = false
-            await fs.writeFile(configPath, JSON.stringify(config, null, 2))
+  constructor(e) {
+    super({
+      name: 'kkkkkk-10086-管理',
+      dsc: 'admin',  
+      event: 'message',
+      priority: 5000,
+      rule: [
+        {
+          reg: '^#*(KKK|kkk|kkkkkk-10086)设置语音盒(开启|关闭)$',  
+          fnc: 'voicebox'
+        },
+        {
+          reg: '^#*(KKK|kkk|kkkkkk-10086)设置(视频解析|解析)(开启|关闭)$',
+          fnc: 'tools' 
+        },
+        {
+          reg: '^#*(KKK|kkk|kkkkkk-10086)设置(默认视频解析|默认解析)(开启|关闭)$',
+          fnc: 'defaulttool'
         }
-    }
+      ]
+    })
+  }
+  
+  async voicebox(e) {
+    const value = getValue(e.msg)
+    await updateConfig('voicebox', value, e)
+  }  
 
-    async tools(e) {
-        if (e.msg.includes('开启')) {
-            const str = await fs.readFile(configPath, 'utf-8')
-            const config = JSON.parse(str)
-            config.videotool = true
-            await fs.writeFile(configPath, JSON.stringify(config, null, 2))
-        } else if (e.msg.includes('关闭')) {
-            const str = await fs.readFile(configPath, 'utf-8')
-            const config = JSON.parse(str)
-            config.videotool = false
-            await fs.writeFile(configPath, JSON.stringify(config, null, 2))
-        }
-    }
+  async defaulttool(e) {
+    const value = getValue(e.msg)
+    await updateConfig('defaulttool', value, e)
+  }
+
+  async tools(e) {
+    const value = getValue(e.msg)
+    await updateConfig('videotool', value, e)
+  }
 
 }
