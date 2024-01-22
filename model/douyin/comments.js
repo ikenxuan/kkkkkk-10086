65fb6dcf1b6eee9618d2ea4301c7cdd45d651e01
ikenxuan <2066855608@qq.com>
2024-01-23 04:28:05 +0800

    feat: 抖音表情包 `@Zyy955`

--- model/douyin/comments.js
+++ model/douyin/comments.js
@@ -1,80 +1,77 @@
 /**
- * 
+ *
  * @param {*} data 完整的评论数据
  * @param {*} emojidata 处理过后的emoji列表
  * @returns obj
  */
 export async function comments(data, emojidata) {
-    let jsonArray = []
+  let jsonArray = [];
 
-    for (let i = 0; i < data.comments.length; i++) {
-        const nickname = data.comments[i].user.nickname
-        const userimageurl = data.comments[i].user.avatar_larger.url_list[0]
-        const text = data.comments[i].text
-        const ip = data.comments[i].ip_label
-        const time = data.comments[i].create_time
-        let digg_count = data.comments[i].digg_count
-        const relativeTime = await getRelativeTimeFromTimestamp(time)
+  for (let i = 0; i < data.comments.length; i++) {
+    const nickname = data.comments[i].user.nickname;
+    const userimageurl = data.comments[i].user.avatar_larger.url_list[0];
+    const text = data.comments[i].text;
+    const ip = data.comments[i].ip_label;
+    const time = data.comments[i].create_time;
+    let digg_count = data.comments[i].digg_count;
+    const relativeTime = await getRelativeTimeFromTimestamp(time);
 
-        const commentObj = {
-            "id": i + 1,
-            "nickname": nickname,
-            "userimageurl": userimageurl,
-            "text": text,
-            "digg_count": digg_count,
-            "ip_label": ip,
-            "create_time": relativeTime
-        };
-        jsonArray.push(commentObj)
+    const commentObj = {
+      id: i + 1,
+      nickname: nickname,
+      userimageurl: userimageurl,
+      text: text,
+      digg_count: digg_count,
+      ip_label: ip,
+      create_time: relativeTime,
+    };
+    jsonArray.push(commentObj);
+  }
+
+  jsonArray.sort((a, b) => b.digg_count - a.digg_count);
+
+  for (let i = 0; i < jsonArray.length; i++) {
+    if (jsonArray[i].digg_count > 10000) {
+      jsonArray[i].digg_count =
+        (jsonArray[i].digg_count / 10000).toFixed(1) + "w";
     }
+  }
 
-    jsonArray.sort((a, b) => b.digg_count - a.digg_count)
 
-    for (let i = 0; i < jsonArray.length; i++) {
-        if (jsonArray[i].digg_count > 10000) {
-            jsonArray[i].digg_count = (jsonArray[i].digg_count / 10000).toFixed(1) + "w"
+  for (const item1 of jsonArray) {
+    // 遍历emojidata中的每个元素
+    for (const item2 of emojidata) {
+      // 如果jsonArray中的text包含在emojidata中的name中
+      if (item1.text.includes(item2.name)) {
+        // 检查是否存在中括号
+        if (item1.text.includes("[") && item1.text.includes("]")) {
+          item1.text = item1.text
+            .replace(/\[[^\]]*\]/g, `&#160<img src="${item2.url}" style="vertical-align: middle; transform: scale(1.1); "/>`)
+            .replace(/\\/g, "")
+        } else {
+          item1.text = `&#160<img src="${item2.url}" style="vertical-align: middle; transform: scale(1.1); "/>`.replace(/\\/g, "")
         }
+      }
     }
-
-    /** md格式替换 */
-    // for (const item1 of jsonArray) {
-    //     // 遍历emojidata中的每个元素
-    //     for (const item2 of emojidata) {
-    //         // 如果jsonArray中的text包含在emojidata中的name中
-    //         if (item1.text.includes(item2.name)) {
-    //             // 将emojidata中对应name的url替换到jsonArray中的text中
-    //             const imageUrl = `!${item2.name}(${item2.url})`
-    //             
-    //             // 检查是否存在中括号
-    //             if (item1.text.includes('[') && item1.text.includes(']')) {
-    //                 // 如果存在，替换现有中括号及其内容
-    //                 item1.text = item1.text.replace(/\[[^\]]*\]/g, imageUrl)
-    //             } else {
-    //                 // 否则，直接替换文本
-    //                 item1.text = item1.text.replace(new RegExp(item2.name, 'g'), imageUrl)
-    //             }
-    //         }
-    //     }
-    // }
-    
-    return jsonArray
+  }
+  return jsonArray;
 }
 
 async function getRelativeTimeFromTimestamp(timestamp) {
-    const now = Math.floor(Date.now() / 1000) // 当前时间的时间戳
-    const differenceInSeconds = now - timestamp
-  
-    if (differenceInSeconds < 60) {
-      return differenceInSeconds + "秒前"
-    } else if (differenceInSeconds < 3600) {
-      return Math.floor(differenceInSeconds / 60) + "分钟前"
-    } else if (differenceInSeconds < 86400) {
-      return Math.floor(differenceInSeconds / 3600) + "小时前"
-    } else if (differenceInSeconds < 2592000) {
-      return Math.floor(differenceInSeconds / 86400) + "天前"
-    } else if (differenceInSeconds < 31536000) {
-      return Math.floor(differenceInSeconds / 2592000) + "月前"
-    } else {
-      return "更久之前"
-    }
+  const now = Math.floor(Date.now() / 1000); // 当前时间的时间戳
+  const differenceInSeconds = now - timestamp;
+
+  if (differenceInSeconds < 60) {
+    return differenceInSeconds + "秒前";
+  } else if (differenceInSeconds < 3600) {
+    return Math.floor(differenceInSeconds / 60) + "分钟前";
+  } else if (differenceInSeconds < 86400) {
+    return Math.floor(differenceInSeconds / 3600) + "小时前";
+  } else if (differenceInSeconds < 2592000) {
+    return Math.floor(differenceInSeconds / 86400) + "天前";
+  } else if (differenceInSeconds < 31536000) {
+    return Math.floor(differenceInSeconds / 2592000) + "月前";
+  } else {
+    return "更久之前";
   }
+}
