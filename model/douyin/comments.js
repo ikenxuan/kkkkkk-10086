4d8617324fe03110b1b3efbf57aeff49272687e0
ikenxuan <2066855608@qq.com>
2024-01-25 23:11:38 +0800

    fix: `fs is not defined`

--- model/douyin/comments.js
+++ model/douyin/comments.js
@@ -7,7 +7,6 @@ import Argument from "./getdata.js";
  * @returns obj
  */
 export async function comments(data, emojidata) {
-  fs.writeFileSync("comments.json", JSON.stringify(data, null, 4));
   let jsonArray = [];
 
   for (let i = 0; i < data.comments.length; i++) {
