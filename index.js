952c564e032d2285416adb3212c106c411d5498a
ikenxuan <2066855608@qq.com>
2024-06-23 18:08:38 +0800

    细节优化

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { logger } from '#lib'
 export const apps = await new Init().load()
 
 logger.info('----------- ₍˄·͈༝·͈˄*₎◞ ̑̑ ----------')
-logger.info(`kkkkkk-10086 ${Version.version}初始化~`)
+logger.info(`${Version.pluginName} ${Version.version}初始化~`)
 logger.info('交流群：795874649')
 logger.info('Created By ikenxuan')
 logger.info('---------------------------------')
