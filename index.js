8567fcec3a3129a1b6cff4f6e151d64e64640df3
ikenxuan <2066855608@qq.com>
2024-06-25 16:31:04 +0800

    update index.js

--- index.js
+++ index.js
@@ -3,8 +3,5 @@ import { logger } from '#lib'
 
 export const apps = await new Init().load()
 
-logger.info('----------- ₍˄·͈༝·͈˄*₎◞ ̑̑ ----------')
-logger.info(`${Version.pluginName} ${Version.version}初始化~`)
+logger.info(`${logger.yellow(`[插件:${Version.version}]`)} ${logger.green(Version.pluginName)} 初始化完成~`)
 logger.info('交流群：795874649')
-logger.info('Created By ikenxuan')
-logger.info('---------------------------------')
