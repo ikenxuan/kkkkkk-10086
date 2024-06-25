a959644e4b4d9fe294a843785e99bcd9e4941c25
ikenxuan <2066855608@qq.com>
2024-06-25 18:24:11 +0800

    fix:ｉｎｉｔ

--- module/init.js
+++ module/init.js
@@ -4,45 +4,35 @@ import { Version } from '#components'
 import { logger } from '#lib'
 
 class Init {
-  async checkCfg () {
+  async checkCfg() {
     const configPath = process.cwd() + `/plugins/${Version.pluginName}/config/config.json`
     const configExampleFile = process.cwd() + `/plugins/${Version.pluginName}/config/config.example.json`
-    fs.access(configPath, fs.constants.F_OK, (err) => {
-      if (err) {
-        fs.copyFile(configExampleFile, configPath, (err) => {
-          if (err) throw err
-          logger.info(logger.green('检测到kkkkkk-10086配置文件不存在，已自动创建 config.json created success'))
-        })
-      }
-    })
-
-    async function updateConfig () {
-      try {
-        const configExample = JSON.parse(fs.readFileSync(process.cwd() + `/plugins/${Version.pluginName}/config/config.example.json`, 'utf8'))
-        const config = JSON.parse(fs.readFileSync(process.cwd() + `/plugins/${Version.pluginName}/config/config.json`, 'utf8'))
-
-        // 找出config_example中config没有的键
-        const newKeys = Object.keys(configExample).filter((key) => !(key in config))
-
-        // 将新的键添加到config中
-        newKeys.forEach((key) => {
-          config[key] = configExample[key]
-        })
-
-        fs.writeFileSync(process.cwd() + `/plugins/${Version.pluginName}/config/config.json`, JSON.stringify(config, null, 2))
-      } catch (error) {
-        console.error('Error updating config:', error)
-      }
+    if (!fs.existsSync(configPath)) {
+      fs.copyFileSync(configExampleFile, configPath)
+      logger.info(logger.green('检测到kkkkkk-10086配置文件不存在，已自动创建 config.json created success'))
     }
 
-    await updateConfig()
+    try {
+      const configExample = JSON.parse(fs.readFileSync(process.cwd() + `/plugins/${Version.pluginName}/config/config.example.json`, 'utf8'))
+      const config = JSON.parse(fs.readFileSync(process.cwd() + `/plugins/${Version.pluginName}/config/config.json`, 'utf8'))
 
-    const videodir = `${Version.pluginPath}/resources/kkkdownload/video`
-    const imgdir = `${Version.pluginPath}/resources/kkkdownload/images`
+      const newKeys = Object.keys(configExample).filter(key => !(key in config))
+
+      newKeys.forEach(key => {
+        config[key] = configExample[key]
+      })
+
+      fs.writeFileSync(process.cwd() + `/plugins/${Version.pluginName}/config/config.json`, JSON.stringify(config, null, 2))
+    } catch (error) {
+      console.error('Error updating config:', error)
+    }
+
+    const videodir = `${Version.clientPath}/resources/kkkdownload/video`
+    const imgdir = `${Version.clientPath}/resources/kkkdownload/images`
     const dirs = [videodir, imgdir]
     for (const dir of dirs) {
       if (!fs.existsSync(dir)) {
-        mkdirs(dir)
+        await mkdirs(dir)
       }
     }
 
@@ -50,7 +40,7 @@ class Init {
     if (!fs.existsSync(readmePath)) {
       fs.writeFileSync(readmePath, '# 这是一个缓存文件夹\n\n## 开源项目[kkkkkk-10086](https://github.com/ikenxuan/kkkkkk-10086)', { flag: 'w' })
     }
-    function mkdirs (dirname) {
+    async function mkdirs(dirname) {
       if (fs.existsSync(dirname)) {
         return true
       } else {
@@ -61,12 +51,12 @@ class Init {
       }
     }
   }
-  async load () {
+  async load() {
     await this.checkCfg()
 
-    const files = fs.readdirSync(`${Version.pluginPath}/apps`).filter((file) => file.endsWith('.js'))
+    const files = fs.readdirSync(`${Version.pluginPath}/apps`).filter(file => file.endsWith('.js'))
     let ret = []
-    files.forEach((file) => {
+    files.forEach(file => {
       ret.push(import(`../apps/${file}`))
     })
 
