166d9259de0fa4b026aafb9e0fe9d5943ee541b3
ikenxuan <2066855608@qq.com>
2024-09-05 17:25:57 +0800

    细节优化

--- apps/tools.js
+++ apps/tools.js
@@ -1,4 +1,4 @@
-import { Config, Pushlist, Version, Base } from '../module/components/index.js'
+import { Config, Pushlist, Version, Base, getMessage } from '../module/components/index.js'
 import { BiLiBiLi, Bilidata, Bilibilipush, GetBilibiliID } from '../module/business/bilibili/index.js'
 import { DouYin, DouYinpush, DouyinData, GetDouyinID } from '../module/business/douyin/index.js'
 import { KuaiShou, GetKuaishouID, KuaishouData } from '../module/business/kuaishou/index.js'
@@ -45,43 +45,7 @@ export class Tools extends plugin {
   }
 
   async prefix (e) {
-    const botAdapter = await new Base(e).botadapter
-    if (Version.BotName === 'Karin') {
-      if (e.reply_id) {
-        const reply = await e.bot.GetMessage(e.contact, e.reply_id)
-        for (const v of reply.elements) {
-          if (v.type === 'text' || v.type === 'json') e.msg = v?.text || v?.data
-          break
-        }
-      }
-    } else {
-      switch (botAdapter) {
-        case 'ICQQ': {
-          if (e.source) {
-            const source = (await e.group.getChatHistory(e.source.seq, 1)).pop()
-            for (const v of source.message) {
-              if (v.type === 'text' || v.type === 'json') e.msg =  v?.text || v?.data
-            }
-          }
-          break
-        }
-        case 'LagrangeCore':
-        case 'Lagrange.OneBot':
-        case 'OneBotv11': {
-          const source = e.message.find(msg => msg.type === 'reply')
-          if (source) {
-            const replyMessage = (await e.bot?.sendApi?.('get_msg', { message_id: source.id }))?.data
-            if (replyMessage?.message) {
-              for (const val of replyMessage.message) {
-                if (val.type === 'text' || val.type === 'json') e.msg = val.data?.text || val.data?.data
-                break
-              }
-            }
-          }
-          break
-        }
-      }
-    }
+    e.msg = await getMessage(e)
 
     if (reg.douyin.test(e.msg)) {
       return await this.douy(e)
--- module/components/index.js
+++ module/components/index.js
@@ -8,8 +8,9 @@ import Pushlist from './Pushlist.js'
 import DB from '../db/index.js'
 import FFmpeg from './FFmpeg.js'
 import Init from '../init.js'
+import getMessage from './getMessage.js'
 
-export { Version, Render, Config, Base, UploadRecord, Networks, Pushlist, DB, Sleep, FFmpeg, Init }
+export { Version, Render, Config, Base, UploadRecord, Networks, Pushlist, DB, Sleep, FFmpeg, Init, getMessage }
 
 /**
  * 休眠函数
