45c50a6d426e2bd5d6fa83761a24e177ab161bf2
OduckO <1564571378@qq.com>
2024-09-24 17:07:02 +0800

    fix: 修复B站动态正则表达式，转义特殊字符

--- module/business/bilibili/push.js
+++ module/business/bilibili/push.js
@@ -589,7 +589,9 @@ function extractEmojisData (data) {
 
 function replacetext (text, obj) {
   for (const tag of obj.modules.module_dynamic.desc.rich_text_nodes) {
-    const regex = new RegExp(tag.orig_text, 'g')
+    // 对正则表达式中的特殊字符进行转义
+    const escapedText = tag.orig_text.replace(/([.*+?^${}()|[\]\\])/g, '\\$1').replace(/\n/g, '\\n')
+    const regex = new RegExp(escapedText, 'g')
     switch (tag.type) {
       case 'RICH_TEXT_NODE_TYPE_TOPIC':
       case 'RICH_TEXT_NODE_TYPE_AT': {
