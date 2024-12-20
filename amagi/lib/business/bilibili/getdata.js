384cf65bd91cdfa688f54631e039d0d9fe4ce7f4
炫炫 <112480306+ikenxuan@users.noreply.github.com>
2024-12-21 06:30:45 +0800

    fix: B站风控校验失败

--- amagi/lib/business/bilibili/getdata.js
+++ amagi/lib/business/bilibili/getdata.js
@@ -6,10 +6,22 @@ export default class BilibiliData {
     URL;
     constructor(type, cookie) {
         this.type = type;
-        this.headers = {};
-        this.headers.Referer = 'https://api.bilibili.com/';
-        this.headers.Cookie = cookie ? cookie.replace(/\s+/g, '') : '';
-        this.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0';
+        this.headers = {
+          'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+          'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+          'cache-control': 'max-age=0',
+          'priority': 'u=0, i',
+          'sec-ch-ua': '\'Microsoft Edge\';v=\'131\', \'Chromium\';v=\'131\', \'Not_A Brand\';v=\'24\'',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '\'Windows\'',
+          'sec-fetch-dest': 'document',
+          'sec-fetch-mode': 'navigate',
+          'sec-fetch-site': 'none',
+          'sec-fetch-user': '?1',
+          'upgrade-insecure-requests': '1',
+          referer: 'https://www.bilibili.com/',
+          cookie: cookie ? cookie.replace(/\s+/g, '') : ''
+        }
     }
     async GetData(data = {}) {
         let result;
