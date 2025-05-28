3967d7f2fcfdaff3d8ba43632cda4a71a1ba91a9
OduckO <78382787+OduckO@users.noreply.github.com>
2025-05-28 16:18:59 +0800

    refactor(platform/douyin): 移除未使用的Emoji模块导出
    清理未使用的Emoji模块导出以简化接口，减少不必要的依赖

--- module/platform/douyin/index.js
+++ module/platform/douyin/index.js
@@ -2,8 +2,7 @@ import DouYin from './douyin.js'
 import DouYinpush from './push.js'
 import DouyinData from './getdata.js'
 import GetDouyinID from './getid.js'
-import Emoji from './emoji.js'
 import { douyinComments } from './comments.js'
 import { dylogin } from './login.js'
 
-export { DouYin, DouYinpush, DouyinData, GetDouyinID, Emoji, douyinComments, dylogin }
+export { DouYin, DouYinpush, DouyinData, GetDouyinID, douyinComments, dylogin }
