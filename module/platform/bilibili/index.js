fc4381318d84b0fecc6487c38dcdf0f20914b805
OduckO <78382787+OduckO@users.noreply.github.com>
2025-08-21 18:31:18 +0800

    refactor(platform/bilibili): 简化登录模块导出结构
    移除未使用的Bilidata模块，并将BiLogin重命名为bilibiliLogin以保持命名一致性

--- module/platform/bilibili/index.js
+++ module/platform/bilibili/index.js
@@ -1,9 +1,8 @@
 import BiLiBiLi from './bilibili.js'
-import Bilidata from './getdata.js'
-import BiLogin from './login.js'
+import { bilibiliLogin } from './login.js'
 import GetBilibiliID from './getid.js'
 import { genParams, checkCk } from './genParams.js'
 import { bilibiliComments } from './comments.js'
 import Bilibilipush from './push.js'
 
-export { BiLiBiLi, Bilidata, BiLogin, GetBilibiliID, bilibiliComments, genParams, checkCk, Bilibilipush }
+export { BiLiBiLi, bilibiliLogin, GetBilibiliID, bilibiliComments, genParams, checkCk, Bilibilipush }
